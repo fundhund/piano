59f0fc1f22c03941a0a7076f68de18a8ecd550a7
fundhund <marius.mielke@googlemail.com>
2022-05-22 21:09:33 +0200

    Added whole notes

--- components/staff/Staff.tsx
+++ components/staff/Staff.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react'
-import { KEYS } from '../../utils/constants'
-import styles from './Staff.module.scss'
 import { Key } from '../../types/piano'
 import { getReversedArray, getSubArray } from '../../utils/arrayHelper'
+import { joinClassNames } from '../../utils/componentHelper'
+import { KEYS } from '../../utils/constants'
+import styles from './Staff.module.scss'
+import WholeNote from './WholeNote'
 
 type Clef = typeof CLEFS[keyof typeof CLEFS]
 
@@ -29,9 +31,11 @@ const POSITIONS = {
 } as const
 
 const NOTES_BY_POSITION = {
-    [POSITIONS.SPACE]: ['D2', 'F2', 'A2', 'C3', 'E3', 'G3', 'B3', 'D4', 'F4', 'A4', 'C5', 'E5', 'G5', 'B5'] as Key[],
+    [POSITIONS.SPACE]: [
+        'D2', 'F2', 'A2', 'C3', 'E3', 'G3', 'B3', 'D4', 'F4', 'A4', 
+        'C5', 'E5', 'G5', 'B5', 'D6', 'F6', 'A6', 'C7', 'E7', 'G7', 'B7'] as Key[],
     [POSITIONS.LINE]: ['G2', 'H2', 'D3', 'F3', 'A3', 'E4', 'G4', 'B4', 'D5', 'F5'] as Key[],
-    [POSITIONS.LEDGER_LINE]: ['C2', 'E2', 'C4', 'A5', 'C6'] as Key[],
+    [POSITIONS.LEDGER_LINE]: ['C2', 'E2', 'C4', 'A5', 'C6', 'E6', 'G6', 'B6', 'D7', 'F7', 'A7', 'C8'] as Key[],
 } as const
 
 const getStaffNotes = (clef: Clef) =>
@@ -56,9 +60,9 @@ const Staff: FC<StaffProps> = ({clef}) => {
                 .map(note => (
                     <div 
                         key={note} 
-                        className={getPositionStyle(note)}
+                        className={joinClassNames(getPositionStyle(note), styles.note)}
                     >
-                        {note}
+                        <WholeNote />
                     </div>
                 ))
             }
---
+++ components/staff/WholeNote.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './WholeNote.module.scss'
+
+const WholeNote = () => (
+    <div className={styles.wholeNote}>
+        <div />
+    </div>
+)
+
+export default WholeNote
